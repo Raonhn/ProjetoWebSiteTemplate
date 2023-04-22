6a00e35b9d6c05019aa630d78395c2f846247ac4
Nathan Ribeiro de Araujo Oliveira <nathan.oliveira@sptech.school>
2023-04-22 01:17:26 -0300

    Site com Banner e headerv1 ok

---
+++ Site/script/slide.js
@@ -0,0 +1,18 @@
+var radio = document.querySelector('.btn_manual')
+var contador = 0
+
+document.getElementById('slide0').checked = true
+
+setInterval(() => {
+   proximaImg() 
+}, 6000)
+
+function proximaImg() {
+    contador += 1
+
+    if(contador > 4){
+        contador = 0
+    }
+
+    document.getElementById('slide'+ contador).checked = true
+}
