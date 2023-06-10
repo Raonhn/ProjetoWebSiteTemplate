fe23944209d42e1cc1bfd09f43cf4be5d1643a70
Nathan Ribeiro de Araujo Oliveira <nathan.oliveira@sptech.school>
2023-06-09 21:06:08 -0300

    desejaveis arrumados

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -1,16 +1,28 @@
 // sessão
 function validarSessao() {
-    // aguardar();
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
     var id = sessionStorage.ID_USUARIO;
 
+    var email2 = localStorage.EMAIL_USUARIO;
+    var nome2 = localStorage.NOME_USUARIO;
+    var id2 = localStorage.ID_USUARIO;
+
     var perfil = document.getElementById("perfil");
 
-    if (email != null && nome != null) {
+    if (email != null && nome != null || email2 != null && nome2 != null) {
         // window.alert(`Seja bem-vindo, ${nome}!`);
-        perfil.innerHTML = nome;
+
+        if(nome != null){
+            perfil.innerHTML = nome;
+        } else {
+            perfil.innerHTML = nome2;
+            sessionStorage.EMAIL_USUARIO = email2;
+            sessionStorage.NOME_USUARIO = nome2;
+            sessionStorage.ID_USUARIO = id2;
+            
+        }
 
         // finalizarAguardar();
     } else {
@@ -19,15 +31,30 @@ function validarSessao() {
     }
 }
 
+function validarSessao2() {
+
+    var email = localStorage.EMAIL_USUARIO;
+    var nome = localStorage.NOME_USUARIO;
+    var id = localStorage.ID_USUARIO;
+
+    if (email != null && nome != null) {
+        alert("Recuperando dados da ultima seção")
+        window.location = "dashboard/cards.html";
+        
+    }
+}
+
 function limparSessao() {
     // aguardar();
     sessionStorage.clear();
+    localStorage.clear();
     // finalizarAguardar();
     window.location = "../cadastro.html";
 }
 
 function avisoSaindo() {
     sessionStorage.clear();
+    localStorage.clear();
     saindo.classList.add('block')
     setTimeout(() => {
         window.location = "../index.html";
